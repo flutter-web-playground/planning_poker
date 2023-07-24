02d8f215df0bf4762643f45681f934af1f59b498
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-07-23 21:16:35 -0300

    feat(Firebase): Included FirebaseOptions
    - Use environment variable

---
+++ lib/core/constants.dart
@@ -0,0 +1,7 @@
+const String apiKey = String.fromEnvironment('APIKEY');
+const String appId = String.fromEnvironment('APPID');
+const String messagingSenderId = String.fromEnvironment('MESSAGINGSENDERID');
+const String projectId = String.fromEnvironment('PROJECTID');
+const String authDomain = String.fromEnvironment('AUTHDOMAIN');
+const String storageBucket = String.fromEnvironment('STORAGEBUCKET');
+const String measurementId = String.fromEnvironment('MEASUREMENTID');
---
+++ lib/firebase_options.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:planning_poker/core/constants.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for android - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: apiKey,
+    appId: appId,
+    messagingSenderId: messagingSenderId,
+    projectId: projectId,
+    authDomain: authDomain,
+    storageBucket: storageBucket,
+    measurementId: measurementId,
+  );
+}
