a0a0fc362282a728bb50f2f7026aa8fa2ce3ef92
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-10-08 09:30:38 -0300

    feat: Changes from CardService
    - Return pass to be List<UserModel>
    - Included param for tableId

--- lib/model/repository/card_repository.dart
+++ lib/model/repository/card_repository.dart
@@ -1,22 +1,22 @@
-import 'package:planning_poker/model/card_model.dart';
 import 'package:planning_poker/model/services/card_service.dart';
+import 'package:planning_poker/model/user_model.dart';
 
 class CardRepository {
   final CardService _service = CardService();
 
-  Stream<CardModel> getTopTable() async* {
-    yield* _service.getTopTable();
+  Stream<List<UserModel>> getTopTable({required String tableId}) async* {
+    yield* _service.getTopTable(tableId: tableId);
   }
 
-  Stream<CardModel> getBottomTable() async* {
-    yield* _service.getBottomTable();
+  Stream<List<UserModel>> getBottomTable({required String tableId}) async* {
+    yield* _service.getBottomTable(tableId: tableId);
   }
 
-  Stream<CardModel> getLeftTable() async* {
-    yield* _service.getLeftTable();
+  Stream<List<UserModel>> getLeftTable({required String tableId}) async* {
+    yield* _service.getLeftTable(tableId: tableId);
   }
 
-  Stream<CardModel> getRightTable() async* {
-    yield* _service.getRightTable();
+  Stream<List<UserModel>> getRightTable({required String tableId}) async* {
+    yield* _service.getRightTable(tableId: tableId);
   }
 }
