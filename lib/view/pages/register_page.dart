09cf991e573069b305b9dcc6a1c91e45e8e27146
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-01-20 11:10:10 -0300

    feat(RegisterPage): Added new page register
    - This page is used to get the table name and set to HomePage

---
+++ lib/view/pages/register_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tableController = TextEditingController(text: '');
+    return Scaffold(
+      body: Center(
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 250,
+                  child: TextFormField(
+                    controller: tableController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      labelText: 'Informe o código da mesa.',
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FilledButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushNamed('/table', arguments: tableController.text);
+                  },
+                  child: const Text('Procurar'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FilledButton(
+                  onPressed: () async {
+                  },
+                  child: const Text('Criar nova mesa'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
