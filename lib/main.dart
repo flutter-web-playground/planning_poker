2ad97c6418611d3d04766b2f3e0c164ad8b4ad08
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-01-20 11:08:26 -0300

    feat(Firebase): Included firebase login
    - Create fake user to control access

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:planning_poker/firebase_options.dart';
 import 'package:planning_poker/view/pages/home_page.dart';
+import 'package:planning_poker/view/pages/register_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final loginInstance = FirebaseAuth.instanceFor(
+    app: Firebase.app(),
+  );
+
+  await loginInstance.createUserWithEmailAndPassword(
+      email: '${loginInstance.hashCode}@email.com', password: '123456');
+
   setUrlStrategy(PathUrlStrategy());
   runApp(const MainApp());
 }
