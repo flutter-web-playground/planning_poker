57887ffc2a62766db96fa1dafdd5316cdd59f4ef
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-01-23 19:01:06 -0300

    feat(SelectedCards): Change to use new widget to selected card
    - Change to statefullwidget to update the state of the cards
    - Surround into a sizedBox to avoid visual changes when cards change his
      sizes and rebuild the page

--- lib/view/widgets/select_cards_widgets.dart
+++ lib/view/widgets/select_cards_widgets.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:planning_poker/core/constants.dart';
-import 'package:planning_poker/view/widgets/front_card_widget.dart';
+import 'package:planning_poker/view/widgets/selectable_card_widget.dart';
 
-class SelectCardWidget extends StatelessWidget {
+class SelectCardWidget extends StatefulWidget {
   const SelectCardWidget({super.key});
+
+  @override
+  State<SelectCardWidget> createState() => _SelectCardWidgetState();
+}
+
+class _SelectCardWidgetState extends State<SelectCardWidget> {
+  int selected = -1;
 
   @override
   Widget build(BuildContext context) {
     return FittedBox(
       fit: BoxFit.fill,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: List.generate(cardList.length, (index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FrontCardWidget(
-              width: kCardWidth,
-              height: kCardHeight,
-              value: cardList[index],
-            ),
-          );
-        }),
+      child: SizedBox(
+        height: kCardHeight * 1.5,
+        width: kCardHeight * cardList.length,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(cardList.length, (index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selected = index == selected ? -1 : index;
+                  });
+                },
+                child: SelectableCardWidget(
+                  selected: index == selected,
+                  card: cardList[index],
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
