e94b14c8fcfaeffcbbf931e68669ba2153f37b5a
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-01-23 18:58:32 -0300

    feat(Widget): Added new widget selectable_card
    - This widget change the size to represent a selected card

---
+++ lib/view/widgets/selectable_card_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:planning_poker/core/constants.dart';
+import 'package:planning_poker/view/widgets/front_card_widget.dart';
+
+class SelectableCardWidget extends StatelessWidget {
+  final String card;
+  final bool selected;
+
+  const SelectableCardWidget({
+    super.key,
+    required this.card,
+    required this.selected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double selectedValue = selected ? 1.3 : 1.0;
+    debugPrint('$selectedValue');
+    return FrontCardWidget(
+      width: kCardWidth * selectedValue,
+      height: kCardHeight * selectedValue,
+      value: card,
+    );
+  }
+}
