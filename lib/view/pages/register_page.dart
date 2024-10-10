84cbf554a47bc80c0343c07f5b179dd6f37d53f4
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-10-09 22:51:37 -0300

    feat(Name field): Add name textformField on register page

--- lib/view/pages/register_page.dart
+++ lib/view/pages/register_page.dart
@@ -6,12 +6,29 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tableController = TextEditingController(text: '');
+    final nameController = TextEditingController(text: '');
+
     return Scaffold(
       body: Center(
         child: Form(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 250,
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      labelText: 'Informe seu nome.',
+                    ),
+                  ),
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: SizedBox(
