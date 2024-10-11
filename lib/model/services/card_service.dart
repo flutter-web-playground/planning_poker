a6f38d88f5e47ffeaed3074f28fe3e36ef090536
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-10-10 23:00:27 -0300

    refactor(Unified): Change to use an unified function to get user tables

--- lib/model/services/card_service.dart
+++ lib/model/services/card_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:planning_poker/model/card_model.dart';
 import 'package:planning_poker/model/user_model.dart';
@@ -5,55 +7,40 @@ import 'package:planning_poker/model/user_model.dart';
 class CardService {
   FirebaseDatabase database = FirebaseDatabase.instance;
 
-  Stream<List<UserModel>> getTopTable({required String tableId}) {
-    return database.ref('room/$tableId/top').onValue.map(
+  Stream<List<UserModel>> getTable({required String tableId, required String side}) {
+    return database.ref('room/$tableId/$side').onValue.map(
       (e) {
         List<UserModel> userModelList = [];
-        for (var value in e.snapshot.children) {
-          UserModel userModel = UserModel(id: '', name: value.key.toString(), card: CardModel(value: value.value.toString()));
+        for (var user in e.snapshot.children) {
+          final userData = json.decode(json.encode(user.value));
+          UserModel userModel = UserModel(
+            id: user.key.toString(),
+            name: userData['name'].toString(),
+            card: CardModel(
+              value: userData['card'].toString(),
+            ),
+          );
           userModelList.add(userModel);
         }
         return userModelList;
       },
     );
+  }
+
+  Stream<List<UserModel>> getTopTable({required String tableId}) {
+    return getTable(tableId: tableId, side: 'top');
   }
 
   Stream<List<UserModel>> getBottomTable({required String tableId}) {
-    return database.ref('room/$tableId/bottom').onValue.map(
-      (e) {
-        List<UserModel> userModelList = [];
-        for (var value in e.snapshot.children) {
-          UserModel userModel = UserModel(id: '', name: value.key.toString(), card: CardModel(value: value.value.toString()));
-          userModelList.add(userModel);
-        }
-        return userModelList;
-      },
-    );
+    return getTable(tableId: tableId, side: 'bottom');
   }
 
   Stream<List<UserModel>> getLeftTable({required String tableId}) {
-    return database.ref('room/$tableId/left').onValue.map(
-      (e) {
-        List<UserModel> userModelList = [];
-        for (var value in e.snapshot.children) {
-          UserModel userModel = UserModel(id: '', name: value.key.toString(), card: CardModel(value: value.value.toString()));
-          userModelList.add(userModel);
-        }
-        return userModelList;
-      },
-    );
+    return getTable(tableId: tableId, side: 'left');
   }
 
   Stream<List<UserModel>> getRightTable({required String tableId}) {
-    return database.ref('room/$tableId/right').onValue.map(
-      (e) {
-        List<UserModel> userModelList = [];
-        for (var value in e.snapshot.children) {
-          UserModel userModel = UserModel(id: '', name: value.key.toString(), card: CardModel(value: value.value.toString()));
-          userModelList.add(userModel);
-        }
-        return userModelList;
-      },
-    );
+    return getTable(tableId: tableId, side: 'right');
+  }
   }
 }
